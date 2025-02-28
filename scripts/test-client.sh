8b8d80b29ffac26076a82d611be11ef593cce7c0
Lewis Revill <lewis1revill@gmail.com>
2025-02-28 17:42:05 +0000

    Make relative paths more robust in testing scripts

--- scripts/test-client.sh
+++ scripts/test-client.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-scriptsdir=$(dirname "$0")
+scriptsdir=$(dirname $(realpath "$0"))
 topdir=$(dirname "$scriptsdir")
 echo
 source "$scriptsdir/BLOCKS.sh"
@@ -9,7 +9,7 @@ cd $topdir/bin/client-eth
 
 failed=0
 for i in ${!blocks[@]}; do
-    cargo +valida run --release -- < ../host/input/1/${blocks[i]}.bin | grep -q "${hashes[i]}";
+    cargo +valida run --release -- < $topdir/bin/host/input/1/${blocks[i]}.bin | grep -q "${hashes[i]}";
     if [ $? -ne 0 ]; then
         echo "block ${blocks[i]} failed"
         failed=1
